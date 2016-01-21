c52a58a7ba85f4d94bc88189fc213fb1e03cb57a
Brian Chung <brian_chung@intuit.com>
2016-01-20 17:13:27 -0800

    Add basic benchmark things

---
+++ benchmark.js
@@ -0,0 +1,9 @@
+var dp = require('deep-freeze');
+console.time('Regular require');
+require('./schema1');
+console.timeEnd('Regular require');
+
+require('./');
+console.time('Deep-frozen require');
+require('./schema2');
+console.timeEnd('Deep-frozen require');
