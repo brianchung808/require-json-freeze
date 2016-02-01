1912987aaa45c55651ece18beb199b31b7fa02df
Brian Chung <brian_chung@intuit.com>
2016-02-01 12:30:07 -0800

    Remove some things

--- benchmark.js
+++
@@ -1,8 +0,0 @@
-console.time('Regular require');
-require('./schema1');
-console.timeEnd('Regular require');
-
-require('./');
-console.time('Deep-frozen require');
-require('./schema2');
-console.timeEnd('Deep-frozen require');
